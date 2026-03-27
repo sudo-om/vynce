47a8fd59bdfbbf4035877f4532cd7ba5461c63ee
sudo-om <ompatil10481@gmail.com>
2026-03-27 14:56:41 +0530

    Initial commit

---
+++ src/main/java/com/ompatil/projects/vynce/VynceApplication.java
@@ -0,0 +1,13 @@
+package com.ompatil.projects.vynce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VynceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VynceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ompatil/projects/vynce/VynceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ompatil.projects.vynce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VynceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
